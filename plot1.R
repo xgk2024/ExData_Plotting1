e07c2fc30e30c885aacf0c37821cc34d3290779e
xgk2024 <162715165+xgk2024@users.noreply.github.com>
2024-03-29 11:58:04 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,16 @@
+
+library(data.table)
+
+# download and extract ZipFile if not already downloaded
+if(!file.exists("data")){ 
+  
+  dataURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  zipFile <- "household_power_consumption.zip"
+  download.file(dataURL, zipFile)
+  unzip(zipFile, files = NULL, list = FALSE, overwrite = TRUE, junkpaths = FALSE, exdir = "./data", unzip = "internal", setTimes = FALSE)
+}
+
+
 dataFile <- "./data/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
